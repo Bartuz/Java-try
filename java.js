e8ddc4ccf2ae7632759b90a259311c60c2eb64a5
Bartuz <iamnotawowplayer@gmail.com>
2013-06-17 16:34:27 -0400

    2nd excercise done

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-recipy = {
+var recipy = {
 	title: "Cocacola",
 	servings: 5,
 	ingredients: ["cukier","slodkosci", "pysznosci"]
@@ -9,3 +9,26 @@ console.log("Skladniki: ")
 for (var i = 0; i<recipy.ingredients.length;i++){
 	console.log(recipy.ingredients[i])
 }
+
+var ksiazki = [
+	{
+		title: "Harry potter",
+		pages: 666,
+		read: false,
+	},
+	{
+		title:"gowno",
+		pages: 0,
+		read: true,
+	}
+];
+
+for (var i = 0; i < ksiazki.length; i++) {
+  var book = ksiazki[i];
+  var bookInfo = book.title + '" with ' + book.pages;
+  if (book.read) 
+  	console.log("NO WAY!")
+  else
+  	console.log(bookInfo)
+ } 
+  
